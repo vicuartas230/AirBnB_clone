9411489fe8f52143b1964896ed57e45b7d61a012
vicuartas230 <vicuartas230@gmail.com>
2021-06-25 22:54:28 +0000

    Adding documentation

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python3
-""" This script defines all common attributes/methods for other classes. """
+""" This script defines a BaseModel class """
 from datetime import datetime
 import uuid
 
 
 class BaseModel():
+    """ This class contains all common attributes/methods
+        for other classes. """
     def __init__(self, id=0, created_at=None, updated_at=None):
+        """ Constructor method to initialize the attribute of the
+            instantiated object with one parameter:
+
+            Parameter:
+
+                id (integer): Is a public instance attribute
+                created_at (datetime): Is a public instance attribute
+                updated_at (datetime): Is a public instance attribute """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
+        """ This method returns a string representation of the instance """
+        return "[{}] ({}) {}\
+".format(__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """ This method updates the date of modification """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        return dict(my_number=getattr(self, "my_number"), name=getattr(self, "name"), id=self.id, created_at=self.created_at.isoformat(), update_at=self.updated_at.isoformat(), __class__=__class__.__name__)
+        """ This method returns a dictionary with the instance attributes """
+        return dict(my_number=getattr(self, "my_number"),
+                    name=getattr(self, "name"), id=self.id,
+                    created_at=self.created_at.isoformat(),
+                    update_at=self.updated_at.isoformat(),
+                    __class__=__class__.__name__)
