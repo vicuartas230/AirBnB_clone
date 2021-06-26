4b07cf7545352569d9a8f2a6a434ad244cb81dc1
vicuartas230 <vicuartas230@gmail.com>
2021-06-26 21:10:08 +0000

    Task 4 done

--- models/base_model.py
+++ models/base_model.py
@@ -7,18 +7,21 @@ import uuid
 class BaseModel():
     """ This class contains all common attributes/methods
         for other classes. """
-    def __init__(self, id=0, created_at=None, updated_at=None):
+    def __init__(self, *args, **kwargs):
         """ Constructor method to initialize the attribute of the
-            instantiated object with one parameter:
-
-            Parameter:
-
-                id (integer): Is a public instance attribute
-                created_at (datetime): Is a public instance attribute
-                updated_at (datetime): Is a public instance attribute """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+            instantiated object with one args and kwars """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    convert = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    value = convert
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ This method returns a string representation of the instance """
@@ -34,5 +37,5 @@ class BaseModel():
         return dict(my_number=getattr(self, "my_number"),
                     name=getattr(self, "name"), id=self.id,
                     created_at=self.created_at.isoformat(),
-                    update_at=self.updated_at.isoformat(),
+                    updated_at=self.updated_at.isoformat(),
                     __class__=__class__.__name__)
