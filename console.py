220f9faaa1c3d029b3ee5d2a7f7b37c612b58719
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-06-27 05:31:25 +0000

    console output

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ HBNH console """
+    prompt = '(hbnb) '
+
+    def do_EOF(self, arg):
+        """Exits console"""
+        return True
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
